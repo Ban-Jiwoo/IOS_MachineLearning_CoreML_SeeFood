354f2a57c22ccb3fb2aa57fe520e88d8ce4713c8
Jiwoo Ban <jban365@gmail.com>
2019-03-07 22:14:31 -0500

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Jiwoo Ban on 3/7/19.
+//  Copyright © 2019 jiwooban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
