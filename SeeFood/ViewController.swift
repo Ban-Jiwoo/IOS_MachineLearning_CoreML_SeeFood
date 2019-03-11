0f8d5ed01869e46a8c5ef0c9ce7caae5e11b02ba
Jiwoo Ban <jban365@gmail.com>
2019-03-10 21:15:52 -0400

    "Getting Image Recognition Results Back from Our V3 Model"
    "V3 모델로 부터 이미지 인식 결과 받기"

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     
+    
+    
     let imagePicker = UIImagePickerController()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +29,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = false
     }
     
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model Failed.")
+            
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    
+        
+    }
+    
+    
+    
+    
+    
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
